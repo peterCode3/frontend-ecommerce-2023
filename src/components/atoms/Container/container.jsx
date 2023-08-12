09a0a3ddee12eaff74f7799ec261ea6695ba8fc7
peterCode3 <codewithpassion3@gmail.com>
2023-08-13 00:50:18 +0400

    Chore: Container component style values space change

--- src/components/atoms/Container/container.jsx
+++ src/components/atoms/Container/container.jsx
@@ -1,6 +1,6 @@
 function Container({ children }) {
     return (
-      <div className="px-[30px] mx-auto 2xl:max-w-[1440px] xl:max-w-[1440px]">{children}</div>
+      <div className="px-[30px] mx-auto xl:max-w-[1440px] 2xl:max-w-[1440px]">{children}</div>
     )
   }
   
